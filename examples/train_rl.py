fe8b7feb878f47ea2adc4532db1b6d0ebb0a8460
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2021-10-20 03:48:34 +0530

    Add: Example for Reinforcement Learning

---
+++ examples/train_rl.py
@@ -0,0 +1,103 @@
+import random
+import gym
+import math
+import numpy as np
+from collections import deque
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
+
+class DQNCartPoleSolver():
+    def __init__(self, n_episodes=1000, n_win_ticks=195, max_env_steps=None, gamma=1.0, epsilon=1.0, epsilon_min=0.01, epsilon_log_decay=0.995, alpha=0.01, alpha_decay=0.01, batch_size=64, monitor=False, quiet=False):
+        self.memory = deque(maxlen=100000)
+        self.env = gym.make('CartPole-v0')
+        if monitor: self.env = gym.wrappers.Monitor(self.env, '../data/cartpole-1', force=True)
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_log_decay
+        self.alpha = alpha
+        self.alpha_decay = alpha_decay
+        self.n_episodes = n_episodes
+        self.n_win_ticks = n_win_ticks
+        self.batch_size = batch_size
+        self.quiet = quiet
+        if max_env_steps is not None: self.env._max_episode_steps = max_env_steps
+
+        # Init model
+        self.model = nn.Sequential(
+            nn.Linear(4,24),
+            nn.Tanh(),
+            nn.Linear(24,48),
+            nn.Tanh(),
+            nn.Linear(48,2)
+        )
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def choose_action(self, state, epsilon):
+        return self.env.action_space.sample() if (np.random.random() <= epsilon) else torch.argmax(self.model(torch.Tensor(state))).item()
+
+    def get_epsilon(self, t):
+        return max(self.epsilon_min, min(self.epsilon, 1.0 - math.log10((t + 1) * self.epsilon_decay)))
+
+    def preprocess_state(self, state):
+        return np.reshape(state, [1, 4])
+
+    def replay(self, batch_size):
+        x_batch, y_batch = [], []
+        minibatch = random.sample(
+            self.memory, min(len(self.memory), batch_size))
+        for state, action, reward, next_state, done in minibatch:
+            y_target = self.model(torch.Tensor(state))
+            y_target[0][action] = reward if done else reward + self.gamma * torch.max(self.model(torch.Tensor(next_state))[0])
+            x_batch.append(torch.Tensor(state[0]))
+            y_batch.append(y_target[0])
+
+        criterion = nn.MSELoss()
+        optimizer = torch.optim.Adam(self.model.parameters(),lr=self.alpha, weight_decay=self.alpha_decay)
+        dataset = TensorDataset(torch.stack(x_batch), torch.stack(y_batch))
+        dataloader = DataLoader(dataset, batch_size=len(x_batch))
+        for _,(x, y) in enumerate(dataloader):
+            y_pred = self.model(x)
+            loss = criterion(y_pred, y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def run(self):
+        scores = deque(maxlen=100)
+
+        for e in range(self.n_episodes):
+            state = self.preprocess_state(self.env.reset())
+            done = False
+            i = 0
+            while not done:
+                action = self.choose_action(state, self.get_epsilon(e))
+                next_state, reward, done, _ = self.env.step(action)
+                next_state = self.preprocess_state(next_state)
+                self.remember(state, action, reward, next_state, done)
+                state = next_state
+                i += 1
+
+            scores.append(i)
+            mean_score = np.mean(scores)
+            if mean_score >= self.n_win_ticks and e >= 100:
+                if not self.quiet: print('Ran {} episodes. Solved after {} trials ✔'.format(e, e - 100))
+                return e - 100
+            if e % 100 == 0 and not self.quiet:
+                print('[Episode {}] - Mean survival time over last 100 episodes was {} ticks.'.format(e, mean_score))
+
+            self.replay(self.batch_size)
+
+        if not self.quiet: print('Did not solve after {} episodes 😞'.format(e))
+        return e
+
+if __name__ == '__main__':
+    agent = DQNCartPoleSolver()
+    agent.run()
