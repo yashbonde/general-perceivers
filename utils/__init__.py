192da0fbb0060375f4817f285d4467dfd2caf2ce
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2022-01-11 13:21:10 +0530

    feat: generation methods

---
+++ utils/__init__.py
@@ -0,0 +1,6 @@
+from . import generation
+
+from .generation import generate_greedy
+from .generation import generate_topk
+from .generation import generate_topP
+from .generation import generate_sample
